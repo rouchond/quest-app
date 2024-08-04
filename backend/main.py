c73d92c54170c694ff00e40f32282df3fd3f79c9
David Rouchon <rouchon576@gmail.com>
2024-08-04 19:58:27 -0400

    added get and create goals routes

--- backend/main.py
+++ backend/main.py
@@ -2,6 +2,38 @@ from flask import request, jsonify
 from config import app, db
 from models import Goal
 
+
+@app.route("/")
+def hello():
+    return "<h1>Hello World</h1>"
+
+
+@app.route("/goals", methods=["GET"])
+def get_goals():
+    goals = Goal.query.all()
+    json_goals = list(map(lambda x: x.to_json(), goals))
+    return jsonify({"goals": json_goals})
+
+@app.route("/create_goal", methods=["POST"])
+def create_goal():
+    name = request.json.get("name")
+    desc = request.json.get("desc")
+    color = request.json.get("color")
+
+    if not name:
+        return(
+            jsonify({"message": "You must include a name"})
+        )
+    
+    new_goal = Goal(goal_name=name, goal_desc=desc, goal_color=color)
+    try: 
+        db.session.add(new_goal)
+        db.session.commit()
+    except Exception as e:
+        return jsonify({"message": str(e)}), 400
+    
+    return jsonify({"message": "Goal Created"}), 201
+
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
